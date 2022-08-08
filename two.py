af95f1d702893906051fe1aa22fc6c2666cb9707
LeMonsalve <devlemonsalve@gmail.com>
2022-08-08 07:25:25 -0500

    Update

--- two.py
+++ two.py
@@ -2,3 +2,7 @@ text = input("Enter a text: ")
 print(text + ": " + str(text.__len__()) + " characters")
 
 text_two = input("Enter a new text: ")
+print(text + " " + text_two)
+
+age = print("Enter your age: ")
+print(text + " " + text_two + "Your age is: " + str(age))
